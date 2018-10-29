1108e80ae83a793088966bd006a157a7c363077d
juanotero17 <40052940+juanotero17@users.noreply.github.com>
2018-10-29 10:16:20 +0100

    Korrektur

--- TankstellProjekt226B/TankstellProjekt226B/Program.cs
+++ TankstellProjekt226B/TankstellProjekt226B/Program.cs
@@ -12,7 +12,7 @@ namespace TankstellProjekt226B
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
-        static void Main()W5SEFU.OPIÜOKLÜKOJHBGVFCDXSYRZEDTFZUGIHJOPKJUHZGTFRDESWA
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
